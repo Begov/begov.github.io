f258f55546d9d83ca66b8d5a79cb8538be135d63
Begov <begov.strelok@gmail.com>
2022-06-17 13:36:45 +0300

    add scroll function

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,20 @@
+const ya = document.querySelector('.ya');
+const infoBlock = document.querySelector('.info-block');
+
+ya.addEventListener('click', function(e) {
+  e.preventDefault();
+  let scrollY = window.pageYOffset;
+  let pageY = infoBlock.getBoundingClientRect().top + scrollY;
+  let interval = setInterval(() => {
+    if(window.pageYOffset + 20 >= pageY) {
+      clearInterval(interval);
+    } else {
+      scrollY += 8;
+      scrollTo(0, scrollY);
+    }
+  }, 5);
+});
+
 $('.reviews').slick({
   dots: true,
   infinite: true,
