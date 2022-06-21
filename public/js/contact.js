7122d0eac0b488003f27bb123d58803e7216dd32
Begov <begov.strelok@gmail.com>
2022-06-21 15:25:59 +0300

    Проверка формы на ошибки

--- public/js/contact.js
+++ public/js/contact.js
@@ -11,16 +11,34 @@ setTimeout(() => {
 }, 0);
 
 submit.addEventListener('click', function(e) {
+
   e.preventDefault();
   let message = '';
+  let sendMessage = true;
+
   [...form.elements].forEach(elem => {
     if(elem.nodeName != 'BUTTON') {
-      message += `${elem.name}: ${elem.value} \n`;
+      if(!elem.value.trim()) elem.classList.add('error');
+      else {
+        elem.classList.remove('error');
+        message += `${elem.name}: ${elem.value} \n`;
+      }
     }
   });
-  fetch(encodeURI(`https://api.telegram.org/bot5467005733:AAFsGUoPRjNmZf_qNzPqEr4H1GE9nu53Wug/sendMessage?chat_id=2021259438&text=${message}`))
-  .then(data => data.json())
-  .then(data => {
-    success.classList.add('show');
-  })
+
+  [...form.elements].forEach(elem => {
+    if(elem.classList.contains('error')) {
+      sendMessage = false;
+      message = '';
+    }
+  });
+
+  if(sendMessage) {
+    fetch(encodeURI(`https://api.telegram.org/bot5467005733:AAFsGUoPRjNmZf_qNzPqEr4H1GE9nu53Wug/sendMessage?chat_id=2021259438&text=${message}`))
+    .then(data => data.json())
+    .then(data => {
+      success.classList.add('show');
+    });
+  }
+
 });
